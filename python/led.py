22bbf341f4a244aa08397b6ec4de54d7380330de
Alessandro Scapin <67109949+scapinalessandro@users.noreply.github.com>
2025-10-24 23:47:58 +0200

    pulito codice generatore immagine

--- python/led.py
+++ python/led.py
@@ -4,10 +4,10 @@ import math
 import socket
 import struct
 
-img = cv2.imread('./rotondo2.png', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('./doraemon2.png', cv2.IMREAD_GRAYSCALE)
 
 # img = cv2.blur(img, (5,5))
-ret,img = cv2.threshold(img,190,255,cv2.THRESH_BINARY)
+ret,img = cv2.threshold(img,220,255,cv2.THRESH_BINARY)
 cv2.imwrite('./output/blurred.jpg', img)
 
 blank = np.ones((700, 700), dtype = np.uint8)
@@ -16,14 +16,16 @@ h, w = img.shape
 
 p = 1
 nleds = 40
-distance = 4.5
+distance = 4
 
 outArray = []
 
 xInitOut = int(w / 2)
 yInitOut = int(h / 2)
 
-cd = 19
+cd = 80
+
+print(int(-1 * 40 * distance) + int(-1 * cd))
 
 for i in range(0, int(360/p)):
     csin = math.sin(math.radians(i * p))
@@ -34,8 +36,8 @@ for i in range(0, int(360/p)):
     for l in range(nleds, 0, -1):
         x = int(ccos * l * distance) + int(ccos * cd)
         y = int(csin * l * distance) + int(csin * cd)
-        
-        bit : int = img[y + yInitOut][x + xInitOut] > 128
+
+        bit : int = img[y + yInitOut][x + xInitOut] < 128
 
         buffer = buffer << 1
         buffer = buffer | (bit & 0x1)
@@ -44,10 +46,11 @@ for i in range(0, int(360/p)):
             outArray.append(buffer)
             print(hex(buffer), end="")
             if(l > 7):
+                pass
                 print(",", end="")
             buffer = 0
 
-        blank[y + yInitOut][x + xInitOut] = img[y + yInitOut][x + xInitOut]
+        blank[y + yInitOut][x + xInitOut] = bit * 128
     print("},")
 
 cv2.imwrite('./output/final.jpg',blank)
