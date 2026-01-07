51c53ee29736feae85e05861aa73b133898fba5f
scap <scapinalessandro2007@gmail.com>
2026-01-07 11:16:05 +0100

    aggiornamento motore.ino

--- motore/motore.ino
+++ motore/motore.ino
@@ -1,20 +1,50 @@
+int speed = 9000;
+bool prevState = 0;
+bool motor = false;
+bool buttonState = false;
+
 void setup() {
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
+  Serial.begin(9600);
 
-  digitalWrite(3,HIGH);   // DIR PIN
-  digitalWrite(4,LOW);    // ENABLE PIN
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, LOW);
 
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+
+  digitalWrite(6,LOW);
   
-  /*
-  for(int i = 0; i < 5000; i++)
-  {
-    tone(2,i);
-    delay(2);
-  }
-*/
-  tone(2,2000);
+  prevState = analogRead(A6) > 800;
+  
+}
 
+int i = 0;
+void loop() {
+  buttonState = analogRead(A6) > 800;
+
+  if (prevState != buttonState && buttonState)
+  {
+    motor = !motor;
   }
-void loop() {}
+  prevState = buttonState;
+
+  if((i < speed) && motor)
+  {
+    digitalWrite(A0, LOW);
+    tone(7,i);
+    delayMicroseconds(500);
+    i+=1;
+  }
+  else if(!motor && i > 0)
+  {
+    tone(7,i);
+    delay(1);
+    i -= 10;
+  }
+  if(i <= 0 && !motor)
+  {
+    digitalWrite(A0, HIGH);
+    tone(7,0);
+    i = 0;
+  }
+}
